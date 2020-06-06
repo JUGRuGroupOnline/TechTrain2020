03f45cb8f8202b74374a44876040eec4794f4430
Kontsevaya Anna <arb9i14@gmail.com>
2020-06-06 22:03:20 +0300

    First task

---
+++ ann.kont/task1/src/main/java/jug/ru/Application.java
@@ -0,0 +1,20 @@
+package ann.kont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/")
+    public String get() {
+        return "Мы";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
