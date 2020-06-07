943a27fadda4b8b7319abeed57ebe519eff11bc4
Ivan Bezyazychnyy <ivan.bezyazychnyy@gmail.com>
2020-06-07 09:48:46 +0300

    Task4

--- ivan-bezyazychnyy/service-4-ruby/app.rb
+++ ivan-bezyazychnyy/service-4-ruby/app.rb
@@ -7,8 +7,8 @@ set :bind, '0.0.0.0'
 get '/' do
     response = Net::HTTP.get_response(URI.parse('http://' + ENV['SOURCE_URL']))
     text = response.body.force_encoding("UTF-8")
-    #if text != "Мы сломали твой" then
-    #    raise "Expected " + "Мы сломали твой" + " but got " + text
-    #end
+    if text != "Мы сломали твой" then
+       raise "Expected " + "Мы сломали твой" + " but got " + text
+    end
     return text + " код,"
 end
