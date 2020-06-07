6b73bc289e25b05cb6df559262d00661d49eb386
ekiv <kessy3@gmail.com>
2020-06-07 08:11:15 +0300

    Восьмая задача. Решение от <ekiv>

---
+++ ekiv/razor/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Result { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Result = await new HttpClient().GetStringAsync("http://localhost:8787");
+            return Page();
+        }
+    }
+}
