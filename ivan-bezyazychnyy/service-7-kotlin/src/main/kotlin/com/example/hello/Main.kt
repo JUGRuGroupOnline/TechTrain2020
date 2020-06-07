71afad7c46a05aea1a8288d0a3aeb3eae8989c72
Ivan Bezyazychnyy <ivan.bezyazychnyy@gmail.com>
2020-06-07 08:32:17 +0300

    Task7

---
+++ ivan-bezyazychnyy/service-7-kotlin/src/main/kotlin/com/example/hello/Main.kt
@@ -0,0 +1,24 @@
+package com.example.hello
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import java.net.URL
+
+fun main(args: Array<String>) {
+   val port = System.getenv("PORT") ?: "8080"
+   embeddedServer(Netty, port.toInt()) {
+       routing {
+           get("/") {
+               var response = URL("http://" + System.getenv("SOURCE_URL")).readText()
+               if (response != "Мы сломали твой код, уронили сервак.") {
+                   throw Exception("Expected " + "Мы сломали твой код, уронили сервак." + " but got " + response)
+               }
+               call.respondText(response + " Вставай, Наташа!", ContentType.Text.Html)
+           }
+       }
+   }.start(wait = true)
+}
